93769cc7204a4f580cabe75218580ace13b59c45
Taiwo <tapiwatiyemba@icloud.com>
2020-02-18 09:58:20 +0000

    addcity

--- src/com/qa/addcity/Runner.java
+++ src/com/qa/addcity/Runner.java
@@ -4,37 +4,27 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
+
 		addcity(test2);
 
 	}
-	
-	static int[] test2 = {1,2,3,4,5,6};
-	static List<Integer> addcity(int[] nums){
-		
-		
+
+	static int[] test2 = { 1, 2, 3, 4, 5, 6 };
+
+	static List<Integer> addcity(int[] nums) {
+
 		ArrayList<Integer> newlist = new ArrayList<Integer>();
+
 		
-		for(int i = 0; i<nums.length; i++) {
-			
-			if(i % 2 ==0) {
-				newlist.add(nums[i]);
-				
-				
-				
-			}
-			
-		}System.out.println(newlist.toString());
-		
-		return newlist;
-		
+
+		return Arrays.stream(nums).boxed().filter(num->num % 2 == 1).collect(Collectors.toList());
+
 	}
 
 }
