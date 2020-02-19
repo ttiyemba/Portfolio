648939b2b51b783ad353f00aae03516aa65022fb
ttiyemba <tapiwatiyemba@icloud.com>
2020-02-19 20:34:34 +0000

    anagram done

---
+++ src/com/qa/anagram/Runner.java
@@ -0,0 +1,42 @@
+package com.qa.anagram;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(anagram("computer","retupmoc"));
+
+	}
+	
+	public static boolean anagram(String a, String b) {
+		
+		if(a.length()!=b.length()) {
+			
+			return false;
+		}else {
+			List<Character> firstlist = new ArrayList<>();
+			List<Character> secondlist = new ArrayList<>();
+			
+			for(int i =0; i<a.length(); i++) {
+				firstlist.add(a.charAt(i));
+				secondlist.add(b.charAt(i));
+			}
+			
+			firstlist.sort(null);
+			secondlist.sort(null);
+			
+			return(firstlist.equals(secondlist));
+			
+		}
+	
+		
+	}
+
+		
+	
+
+}
