179996a2cb2a429df94725af2e837ffa3975a55e
Tapiwa <tapiwa@email.com>
2020-02-21 09:39:44 +0000

    fib

--- src/com/qa/fib/Runner.java
+++ src/com/qa/fib/Runner.java
@@ -6,5 +6,31 @@ public class Runner {
 		// TODO Auto-generated method stub
 
 	}
+	
+	 static int fibonacci(int num) {
+		 if(num ==0) {
+			 return 0;
+		 }
+		 int firstpos = 0;
+		 int nextpos = 1;
+		 
+		 for(int i = 0; i<num -1; i++) {
+			 int temp = firstpos;
+			 firstpos = nextpos;
+			 nextpos = firstpos+temp;
+		 }
+		 return nextpos;
+		
+	}
+	 
+	 static int fibanacci2(int num) {
+		 if(num == 0) {
+			 return 0;
+		 }else if (num == 1) {
+			 return 1;
+		 }else {
+			 return fibanacci2(num-1) + fibanacci2(num-2);
+		 }
+	 }
 
 }
